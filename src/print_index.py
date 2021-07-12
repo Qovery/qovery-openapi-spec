330949624e6cee132d4c8a57367926468a13f1c8
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2021-07-12 10:16:24 +0200

    fix: extract objects / other fixes

--- src/print_index.py
+++ src/print_index.py
@@ -5,6 +5,8 @@ from os.path import isfile, join
 def index(dir):
   files = [f for f in listdir(dir) if isfile(join(".", f))]
   for f in files:
+    if "_index" in f:
+      continue
     stripped = f.replace(".yaml", "")
     print(stripped + ":")
     print(" $ref: ./" + f)
