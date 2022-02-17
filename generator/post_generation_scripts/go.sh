6f28d532f8967decc46ed9dd6be8aab89f4c21e7
Bilel Benamira <bbenamira@qovery.com>
2022-02-17 13:40:13 +0100

    chore: update script

--- generator/post_generation_scripts/go.sh
+++ generator/post_generation_scripts/go.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sed -i 's/AnyOfstringboolean/interface{}/g' $(find out/client/ -type f)
-gofmt -s -w -l out/client/
+sed -i 's/AnyOfstringboolean/interface{}/g' $(find out/qovery-client-go/ -type f)
+gofmt -s -w -l out/qovery-client-go/
