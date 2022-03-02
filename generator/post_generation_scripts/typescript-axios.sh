54124fe31e5644609540c081e6f1b2611436e526
Bilel Benamira <bbenamira@qovery.com>
2022-03-02 16:03:25 +0100

    fix: typescript axios post script

--- generator/post_generation_scripts/typescript-axios.sh
+++ generator/post_generation_scripts/typescript-axios.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sed -i 's/name: "RequiredError"/override name: "RequiredError"/g' out/base.ts
+sed -i 's/name: "RequiredError"/override name: "RequiredError"/g' $(find out/qovery-client-typescript-axios/base.ts -type f)
