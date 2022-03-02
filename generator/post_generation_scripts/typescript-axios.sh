23876ba0a94f0464537a3dee26a515f48cf1bea7
Bilel Benamira <bbenamira@qovery.com>
2022-03-02 17:44:33 +0100

    fix: axios post gen script

--- generator/post_generation_scripts/typescript-axios.sh
+++ generator/post_generation_scripts/typescript-axios.sh
@@ -2,4 +2,5 @@
 sed -i 's/name: "RequiredError"/override name: "RequiredError"/g' $(find out/qovery-client-typescript-axios/base.ts -type f)
 cp -r generator/files/typescript-axios/.github out/qovery-client-typescript-axios/
 cd out/qovery-client-typescript-axios
-npm i
+npm install
+npm run build
