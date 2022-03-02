6e11069060cb1c3e126d204447fd1a8c85fe520a
Bilel Benamira <bbenamira@qovery.com>
2022-03-02 16:29:17 +0100

    chore: add npm github action

--- generator/post_generation_scripts/typescript-axios.sh
+++ generator/post_generation_scripts/typescript-axios.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 sed -i 's/name: "RequiredError"/override name: "RequiredError"/g' $(find out/qovery-client-typescript-axios/base.ts -type f)
+cp -r generator/files/typescript-axios/.github out/qovery-client-typescript-axios/
