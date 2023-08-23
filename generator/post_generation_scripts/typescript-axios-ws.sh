f4f5f83ea0bdc9c20d57fb6d0e81014bec12ce14
GitHub Action <action@github.com>
2023-08-23 17:00:09 +0200

    Fix github action

--- generator/post_generation_scripts/typescript-axios-ws.sh
+++ generator/post_generation_scripts/typescript-axios-ws.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-sed -i 's/name: "RequiredError"/override name: "RequiredError"/g' "$(find out/qovery-client-typescript-axios/base.ts -type f)"
-sed -i 's/"^3.6.4"/"~4.5.2"/g' "$(find out/qovery-client-typescript-axios/package.json -type f)"
-cp -r generator/files/typescript-axios/.github out/qovery-client-typescript-axios/
-cd out/qovery-client-typescript-axios || exit
+sed -i 's/name: "RequiredError"/override name: "RequiredError"/g' "$(find out/qovery-client-ws-typescript-axios/base.ts -type f)"
+sed -i 's/"^3.6.4"/"~4.5.2"/g' "$(find out/qovery-client-ws-typescript-axios/package.json -type f)"
+cp -r generator/files/typescript-axios/.github out/qovery-client-ws-typescript-axios/
+cd out/qovery-clientws-typescript-axios || exit
 npm install
 npm run build
