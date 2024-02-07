3a9c02ce408f8753f6f23acf9493ae09482fdda1
GitHub Action <action@github.com>
2024-02-07 09:55:19 +0100

    Bump openapi-generator

--- generator/post_generation_scripts/go.sh
+++ generator/post_generation_scripts/go.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+set -x
+
 sed -i 's/go 1.13/go 1.18/g' "$(find out/qovery-client-go/ -type f)"
-cd out/qovery-client-go || exit ; go get ; go mod tidy ; gofmt -s -w .
+cd out/qovery-client-go || exit 
+sed -i 's/"bytes"//' model_job_response.go model_cluster_request_features_inner_value.go
+go get
+go mod tidy
+gofmt -s -w .
 go build
