439bb70fea72c681ed2caadd537bd37bf263f6f3
ctjhoa@qovery.com <ctjhoa@qovery.com>
2024-04-29 19:08:44 +0200

    Modified 4 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI0NDY1MQ/spectral.js?branch_slug=stoplight&token=088add2b-ea84-490c-80a5-e0a1c4118e56";
+export default { extends: ruleset };
