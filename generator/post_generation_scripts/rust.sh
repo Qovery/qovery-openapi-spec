a8f1736f4417e4f1c48b3622dc5275f9e2bd5585
Erèbe - Romain Gerard <github@erebe.eu>
2025-06-03 10:45:35 +0200

    set proper discriminator for service list (#832)

--- generator/post_generation_scripts/rust.sh
+++ generator/post_generation_scripts/rust.sh
@@ -1,1 +1,11 @@
 #!/bin/sh
+
+cd out/qovery-client-rust || exit
+
+for p in $(ls --color=never ../../generator/patches/rust/*.diff)
+do
+  echo "Applying patch $p"
+  patch -p1 < $p
+done
+cargo build
+cargo fmt
